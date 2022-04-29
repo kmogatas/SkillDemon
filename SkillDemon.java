6e93d1bd6b6df2b6b3fa9279280a2400683288f3
kmogatas <kaleighrossmogatas@gmail.com>
2022-04-29 13:09:42 -0700

    Added Test file

--- SkillDemon.java
+++ SkillDemon.java
@@ -1,5 +1,19 @@
 public class SkillDemon{
 
-// VSCode
+    public static int basicallyMultiplication(int num, int num2){
+
+        int total = 0;
+
+        for (int i = 0; i < 5; i++){
+
+            total = (num + num2);
+
+        }
+
+        return total; 
+    }
+
+    // There is a bug in this, causing our test to fail.
+
 
 }
---
+++ SkillDemonTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonTest {
+    
+    @Test 
+    public void basicallyMultiplication(){
+        assertEquals(15, SkillDemon.basicallyMultiplication(1, 2));
+    }
+    // Initially fail.
+}
